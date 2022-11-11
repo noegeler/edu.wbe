a633131032680d5c2ef9cc02d835df2713db12f0
lichtnoa <noah.lichtenecker@gmail.com>
2022-11-11 15:02:01 +0100

    Update main.js
    Add functionality to random add a piece on the grid

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
-const boardWidth = 6
-const boardHight = 7
+const boardWidth = 7
+const boardHight = 6
 
+let state = {board: undefined}
+loadEmptyBoardState()
 
 function elt(type, attrs, ...children) {
     let node = document.createElement(type)
@@ -18,7 +20,7 @@ function elt(type, attrs, ...children) {
 }
 
 function showBoard() {
-    let container = Array.from(document.getElementsByClassName('board'))[0]
+    let container = document.querySelector('.board')
     container.innerHTML = ""
 
     createBoard(container)
@@ -30,23 +32,51 @@ function createBoard(container) {
 
     for (let i = 0; i < boardHight; i++) {
         for (let j = 0; j < boardWidth; j++) {
-            let piece = generatePiece()
+            let piece = createPiece(state.board[i][j])
             let field = elt('div', attr, piece)
             container.appendChild(field)
         }
     }
 }
 
+function loadEmptyBoardState() {
+    state.board = Array(boardHight).fill('').map(el => Array(boardWidth).fill(''))
+}
+
+function createPiece(state) {
+    let piece = ''
+
+    if (state !== '') {
+        piece = elt('div', {'class': 'piece'}, '')
+        if (state === 'b') {
+            piece.classList.add('playerTwo')
+        } else if (state === 'r') {
+            piece.classList.add('playerOne')
+        }
+    }
+
+    return piece
+}
+
 function generatePiece() {
-    let playerOne = elt('div', {'class': 'piece'}, '')
-    let playerTwo = playerOne.cloneNode()
-    playerOne.classList.add('playerOne')
-    playerTwo.classList.add('playerTwo')
     let choices = [
         '',
-        playerOne,
-        playerTwo
+        'r',
+        'b'
     ]
 
     return choices[Math.floor(Math.random()*choices.length)];
 }
+
+function setNewRandomPiece() {
+    let piece = generatePiece()
+    let posHight = Math.floor(Math.random() * boardHight)
+    let posWidth = Math.floor(Math.random() * boardWidth)
+
+    state.board[posHight][posWidth] = piece
+}
+
+setInterval(function () {
+    setNewRandomPiece()
+    showBoard()
+}, 1000)
